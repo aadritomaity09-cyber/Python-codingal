87721335421653ee21095887f9afe89add491179
aadritomaity09-cyber <aadrito.maity09@gmail.com>
2026-02-21 19:58:30 +0100

    Add files via upload

---
+++ ClothesCommander.py
@@ -0,0 +1,5 @@
+w= float(input("Enter the tempurature at your place:"))
+if w>20:
+     print("You should wear a vest and sweater or just a t-shirt")
+else:
+     print("You should wear a t-shirt and sweater or t-shirt and jacket")
