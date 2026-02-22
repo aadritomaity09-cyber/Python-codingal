144e10500799f37b05769d566e2a6ac5162abdef
aadritomaity09-cyber <aadrito.maity09@gmail.com>
2026-02-22 12:06:12 +0100

    Add files via upload

---
+++ Lesson6.py
@@ -0,0 +1,19 @@
+a=1
+b=0
+c=1
+if a and b and c:
+    print("All the numbers have boolean values")
+else:
+    print("At least one of the numbers has a boolean value")
+
+
+d=0
+e=0
+f=1
+if d or e or f:
+    print("All the numbers have boolean values")
+else:
+    print("At least one of the numbers has a boolean value")
+
+
+    
