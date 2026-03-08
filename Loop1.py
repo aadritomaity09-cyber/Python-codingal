5e029ffaea22e4b47f4fa5bdd1de73a171b18353
aadritomaity09-cyber <aadrito.maity09@gmail.com>
2026-03-08 12:17:57 +0100

    Add files via upload

---
+++ Loop1.py
@@ -0,0 +1,23 @@
+n=(input("Enter your number:"))
+for i in n:
+    print(i,end="½§¤µ")
+
+
+n=int(input("Enter your number:"))
+for i in range(n):
+    print(i)
+
+n=int(input("Enter your number:"))
+for i in range(9):
+    print(i)
+
+n=int(input("Enter your number:"))
+for i in range(9):
+    print(i)
+
+
+n=int(input("Enter your number:"))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+    print(sum)
---
+++ ReversedString.py
@@ -0,0 +1,8 @@
+r=input("Enter a string==")
+t=""
+for i in r:
+    t=i+t
+print(t)
+
+y=r[::-1]
+print(y)
