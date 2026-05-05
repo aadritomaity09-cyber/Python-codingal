90d2ee830e48123a6069aac3961bb4120a0347f1
aadritomaity09-cyber <aadrito.maity09@gmail.com>
2026-05-05 16:43:25 +0200

    Add files via upload

---
+++ nowOrNever.py
@@ -0,0 +1,5 @@
+m=float(input("You will now pay 20% of your bill. what is the total bill amount?=  "))
+a=(m/100)*20
+print("You will now pay ",a,"$")
+g=m-a
+print("You have 4 weeks to pay ",g,"$")
