c7569adc100ab4d334b4a5698ffea178293da815
aadritomaity09-cyber <aadrito.maity09@gmail.com>
2026-05-08 13:20:54 +0200

    Add files via upload

---
+++ MirroredRightAngleTriangle.py
@@ -0,0 +1,3 @@
+n =int(input("Enter the size:"))  # Height
+for i in range(1, n + 1):
+    print(f"{'*' * i:>{n}}")
---
+++ SquareRoot.py
@@ -0,0 +1,3 @@
+x=int(input("Enter a number from 1 to 10 to have the square root " ))
+y=x**0.5
+print("The square root of ",x,"is",y,)
