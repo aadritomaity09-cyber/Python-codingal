723bdbc8fc3062e7c114068d0f6b5348609ca1fd
aadritomaity09-cyber <aadrito.maity09@gmail.com>
2026-06-28 20:20:12 +0200

    Update birthday list and change last date

--- BirthdayStorer.py
+++ BirthdayStorer.py
@@ -1,4 +1,4 @@
-valueslist = ("4/4/1985", "25/1/1987", "10/11/1989", "24/1/2010", "7/12/2015")
+valueslist = ("4/4/1985", "25/1/1987", "10/11/1989", "24/1/2010", "25/12/2026")
 choice = int(input("Which item number do you want to see (1-5)? "))
 try:
     print("The value is:", valueslist[choice - 1])
